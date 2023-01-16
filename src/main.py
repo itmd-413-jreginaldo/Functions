8b348805b30bd5f5ee8cc46013d39778dea7fa5d
Josh R <jreginaldo@protonmail.com>
2023-01-16 12:34:39 -0600

    Add module imports into main driver file

---
+++ src/main.py
@@ -0,0 +1,26 @@
+"""
+Python Functions
+This program will explore the use of functions and file modules
+
+Question 1:
+Create two functions that convert from radians to degrees (r2d(x)) and from degrees to
+radians (d2r(x)) respectively. Save your functions into a separate Python file and import
+the file when you write your main program.
+
+Question 2:
+Given the complete program, refactor the program to appropriately implement the use of functions
+
+Author: Josh Reginaldo
+https://github.com/itmd-413-jreginaldo/Functions
+"""
+
+import question_one
+import question_two
+
+
+def main():
+    print("Hello World")
+
+
+if __name__ == '__main__':
+    main()
